396ab2926a5642784e3f984216eb989e0cfd2c05
Master <mytimenow101@gmail.com>
2019-08-11 18:39:32 -0400

    added viewsets

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializer
 
@@ -50,3 +51,47 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object from table"""
         return Response({'delete':'DELETE'})
+
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSets"""
+    def list(self, request):
+        a_viewset = [
+                'Uses actions (list, create , retrieve, update, partial_update)',
+                'Automatically maps to URLS using Routers',
+                'Provides more functionality with less code',
+        ]
+        return Response({'message':'Hellow!', 'a_viewset':a_viewset})
+
+
+   def create (self, request):
+       """Create a new hello message"""
+       serializer = self.serializer_class(data=request.data)
+
+       if serializer.is_valid():
+           name = serializer.validated_data.get('name')
+           message = f'Hello {name}'
+           return Response({'message':message})
+       else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+           )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its id"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an obect"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating a part of the obect"""
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Distroys the object"""
+        return Response({'http_method': 'DELETE'})      
